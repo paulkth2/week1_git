24bc53fcc42e2b937e7ba51861f5d0e113f8049c
seoyoniee <seoyoniee22@gmail.com>
2018-12-30 22:21:40 +0900

    tap3

---
+++ app/src/main/java/com/example/taehyungkim/viewpagertut/YouTubeVideos.java
@@ -0,0 +1,25 @@
+package com.example.taehyungkim.viewpagertut;
+
+public class YouTubeVideos {
+
+    String videoUrl;
+
+    public YouTubeVideos() {
+
+    }
+
+    public YouTubeVideos(String videoUrl)
+    {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getVideoUrl()
+    {
+        return videoUrl;
+    }
+
+    public void  setVideoUrl(String videoUrl)
+    {
+        this.videoUrl = videoUrl;
+    }
+}
